afc4c8542c6f2d6ab1758f74ffb638c9768fb4e6
tn-als <kusumin38@gmail.com>
2025-11-03 20:31:59 +0900

    refactor: seperate terms template

--- src/main/java/kr/kakao_tech_bootcamp/community/controller/user/TermsController.java
+++ src/main/java/kr/kakao_tech_bootcamp/community/controller/user/TermsController.java
@@ -2,15 +2,24 @@ package kr.kakao_tech_bootcamp.community.controller.user;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/terms")
+@RequestMapping("/agree")
 public class TermsController {
-    @GetMapping
+    @GetMapping("/terms")
     @Operation(summary = "약관 동의")
-    public String agreeTemplate() {
+    public String terms(Model model) {
+        model.addAttribute("companyName", "NodeUp");
         return "agree-terms";
+    }
+
+    @GetMapping("/privacy")
+    @Operation(summary = "개인정보동의")
+    public String privacy(Model model) {
+        model.addAttribute("companyName", "NodeUp");
+        return "agree-privacy";
     }
 }
