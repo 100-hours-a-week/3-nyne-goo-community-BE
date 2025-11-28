072c107c362332816163dd7ce9c88b1a0195657b
tn-als <kusumin38@gmail.com>
2025-11-28 10:52:02 +0900

    feat: add h2 db and make application-test.yml

--- src/test/java/kr/kakao_tech_bootcamp/community/CommunityApplicationTests.java
+++ src/test/java/kr/kakao_tech_bootcamp/community/CommunityApplicationTests.java
@@ -2,8 +2,10 @@ package kr.kakao_tech_bootcamp.community;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")     // application-test.yml 설정 우선 적용
 class CommunityApplicationTests {
 
 	@Test
--- src/test/java/kr/kakao_tech_bootcamp/community/service/user/UserServiceTest.java
+++ src/test/java/kr/kakao_tech_bootcamp/community/service/user/UserServiceTest.java
@@ -15,15 +15,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
