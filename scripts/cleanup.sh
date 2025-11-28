4d8fe923f1e3cdd54142ecf39aeb674caeeeb8b3
tn-als <kusumin38@gmail.com>
2025-11-28 17:31:13 +0900

    fix: add file removing docker-compose

---
+++ scripts/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+rm -f /home/ubuntu/dorandoran-be/docker-compose.yml
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e # error 시 즉시 종료
 
+ECR_REGISTRY=586421527844.dkr.ecr.ap-northeast-2.amazonaws.com
 echo "[deploy.sh] start"
 
 APP_DIR="$HOME/dorandoran-be"
@@ -10,7 +11,7 @@ cd "$APP_DIR"
 
 # ECR 로그인
 echo "[deploy.sh] ECR login..."
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 586421527844.dkr.ecr.ap-northeast-2.amazonaws.com
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin $ECR_REGISTRY
 
 docker compose pull
 docker compose up -d
